1d4d56432ecfce192d2f12be56c279218c4b51e2
hagi <hahagigi77@gmail.com>
2020-07-30 03:05:17 +0900

    プロジェクト ファイルを追加します。

---
+++ C_Sharp/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_Sharp
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
